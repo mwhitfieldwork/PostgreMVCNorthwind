9860c4006d7c5761ddb802b15d2e330bc6ee2f70
mwhitfieldwork <mwhitfieldwork@gmail.com>
2026-08-05 16:05:27 -0500

    Fix program cs for production

--- NWCodeFirstMVC.Api/Program.cs
+++ NWCodeFirstMVC.Api/Program.cs
@@ -69,11 +69,9 @@ NLog.LogManager.Setup().LoadConfiguration(builder => {
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
